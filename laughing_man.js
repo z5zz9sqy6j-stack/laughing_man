54ffb39e428d5cc8b89c25dd86dd30d310a61490
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-04-22 10:38:27 -0400

    added starting movement

--- laughing_man.js
+++ laughing_man.js
@@ -1,20 +1,39 @@
 const SQUARE_COUNT = 3;
-
+const TIMER_SPEED = 16.6;
 document.addEventListener('DOMContentLoaded', () => {
+ document.querySelector(".square").addEventListener("click", () => {
+            alert("OMG YOU CLICKED ME!");
+        });
+
+
 
     let box = document.querySelector("#box");
 
     for (let i = 0; i < SQUARE_COUNT; i++) {
+
         let square = document.createElement('img');
+
         square.src = "laughing_man.jpg";
         square.alt = "Catch the Laughing Man!";
         square.className = "square";
-
-        square.addEventListener("click", () => {
-            alert("OMG YOU CLICKED ME!");
-        });
-
         box.appendChild(square);
     }
 
+    Array.from((box.children)).forEach(() => {
+
+        let dx=  5 * Math.random() * 2- 1;
+        let dy=  5 * Math.random() * 2- 1;
+        
+
+        let x = parseInt(Element.style.left) || 0;
+        let y = parseInt(Element.style.top) || 0;
+        setInterval(() => {
+        x += dx;
+        y += dy;
+    
+        element.style.left = x + "px";
+        element.style.top = y + "px";
+
+} , TIMER_SPEED) 
+    });     
 });
